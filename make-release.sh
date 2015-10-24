fd69c70890a648b3621a72ea63d3af17e21e5db5
Daniel Serpell <daniel.serpell@gmail.com>
2015-10-24 20:16:39 -0300

    Adds script for compiling releases.
    Signed-off-by: Daniel Serpell <daniel.serpell@gmail.com>

---
+++ make-release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Script to build release tarballs
+# --------------------------------
+#
+# Compiles for three different platforms:
+# - For Win32, using the mingw-w64 compiler,
+# - For Linux 64bit PC, using GCC,
+# - For Linux 32bit PC, using GCC.
+#
+# Uses link-time-optimizations and disable asserts.
+
+
+out="basicParser-$(git describe --long --dirty)"
+
+rm -rf build/
+make CROSS=i686-w64-mingw32- EXT=.exe CFLAGS='-Wall -std=c99 -O2 -flto -DNDEBUG' dist
+mv build/${out}.zip ../${out}-win32.zip
+
+rm -rf build/
+make CROSS= EXT= CFLAGS='-Wall -O2 -flto -DNDEBUG' dist
+mv build/${out}.zip ../${out}-linux64.zip
+
+rm -rf build/
+make CROSS= EXT= CFLAGS='-Wall -m32 -O2 -flto -DNDEBUG' dist
+mv build/${out}.zip ../${out}-linux32.zip
+
